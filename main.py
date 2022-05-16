77afa01e0a2378a056e5eb52f47f37a474103c54
Random <78389553+Sam772@users.noreply.github.com>
2022-05-16 15:55:39 +0100

    Remove unneeded file

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ import pandas as pd
 # reading tornado dataset
 dataset = pd.read_csv('dataset/2011Tornado_Summary.csv')
 print(dataset)
+
+keywords = ['storm', 'wildfire', 'blizzard', 'hurricane', 'flood']
